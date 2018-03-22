05f07b6329e4eda725f657d8ecca9c719a656204
Jed Darrohn <jamesdarrohn@gmail.com>
2018-03-22 15:50:15 -0600

    Beginning to build out header

--- docroot/content/themes/ddevcom_theme/templates/header.php
+++ docroot/content/themes/ddevcom_theme/templates/header.php
@@ -1,8 +1,8 @@
 <!-- Just an image -->
-<nav class="navbar navbar-dark bg-primary">
+<nav class="navbar navbar-dark bg-primary fixed-top">
   <a class="navbar-brand" href="#">
     <img src="/content/themes/ddevcom_theme/dist/images/ddev-logo.svg" width="200" alt="">
   </a>
 
-  <a href="#" class="btn btn-secondary ml-auto">Get Started</a>
+  <a href="#" class="btn btn-secondary btn-lg ml-auto">Get Started</a>
 </nav>
