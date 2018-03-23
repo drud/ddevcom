65670071e16ae25b2501d676b892be8a6842923b
Jed Darrohn <jamesdarrohn@gmail.com>
2018-03-22 20:36:30 -0600

    Added WP CLI Login. Main navigation menu. Continuing to build out front page. :neckbeard:

--- docroot/content/themes/ddevcom_theme/templates/header.php
+++ docroot/content/themes/ddevcom_theme/templates/header.php
@@ -3,9 +3,16 @@
   <?php if (is_admin_bar_showing()): ?>
     <div style="height: 32px;"></div>
   <?php endif; ?>
+
   <a class="navbar-brand" href="#">
     <img src="/content/themes/ddevcom_theme/dist/images/ddev-logo.svg" width="200" alt="">
   </a>
+
+  <?php
+    if (has_nav_menu('primary_navigation')) :
+      wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+    endif;
+   ?>
 
   <a href="#" class="btn btn-secondary btn-lg d-none d-md-block ml-auto">Get Started</a>
 </nav>
