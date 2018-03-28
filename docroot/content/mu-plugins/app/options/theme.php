1629cb5ef2a83cb6e878c6e1cd28642e44b64fd9
Jed Darrohn <jamesdarrohn@gmail.com>
2018-03-28 09:31:40 -0600

    Created Page Header ACF group. Adding conditionals to mu plugins for deployment.

--- docroot/content/mu-plugins/app/options/theme.php
+++ docroot/content/mu-plugins/app/options/theme.php
@@ -3,11 +3,13 @@
 namespace App\Options\Theme;
 
 add_action('init', function(){
-  acf_add_options_page([
-    'page_title'   => 'Theme Settings',
-    'menu_title'  => 'Theme Settings',
-    'menu_slug'   => 'theme-general-settings',
-    'capability'  => 'edit_posts',
-    'redirect'    => true
-  ]);
+  if (function_exists('acf_add_options_page')) {
+    acf_add_options_page([
+      'page_title'   => 'Theme Settings',
+      'menu_title'  => 'Theme Settings',
+      'menu_slug'   => 'theme-general-settings',
+      'capability'  => 'edit_posts',
+      'redirect'    => true
+    ]);
+  }
 });
