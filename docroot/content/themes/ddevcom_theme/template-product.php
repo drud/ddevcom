5fb43e5944544be8cab7bccc4f5f991f6f30c5bf
Jed Darrohn <jamesdarrohn@gmail.com>
2018-04-05 09:31:50 -0600

    Adjustments on product page and get started. Added source sans

--- docroot/content/themes/ddevcom_theme/template-product.php
+++ docroot/content/themes/ddevcom_theme/template-product.php
@@ -66,20 +66,22 @@
                 <div class="row">
                   <div class="col-lg-12 col-xl-10 mx-auto">
                     <div class="row">
-                      <div class="col-lg-6 px-0">
+                      <div class="col-lg-3 px-0">
                         <img src="<?php the_sub_field('image'); ?>" alt="" class="img-fluid">
                         <p class="small mt-2"><?php the_sub_field('image_attribution'); ?></p>
                       </div>
-                      <div class="col-lg-6">
+                      <div class="col-lg-9">
                         <div class="py-5 px-lg-5">
                           <h4 class="text-primary h3 my-4 mt-lg-0"><?php the_sub_field('header'); ?></h4>
                           <div class="text-muted mb-5">
                             <?php the_sub_field('content'); ?>
                           </div>
                           <?php $section_left_link = get_sub_field('link'); ?>
-                          <a href="<?= $section_left_link['url']; ?>" class="btn btn-outline-primary-light btn-lg mb-4 mb-lg-0">
-                            <?= $section_left_link['title']; ?>
-                          </a>
+                          <?php if($section_left_link): ?>
+                            <a href="<?= $section_left_link['url']; ?>" class="btn btn-outline-primary-light btn-lg mb-4 mb-lg-0">
+                              <?= $section_left_link['title']; ?>
+                            </a>
+                          <?php endif; ?>
                         </div>
                       </div>
                     </div>
@@ -90,20 +92,22 @@
                 <div class="row">
                   <div class="col-lg-12 col-xl-10 mx-auto">
                     <div class="row">
-                      <div class="col-lg-6 order-lg-2 px-0">
+                      <div class="col-lg-3 order-lg-2 px-0">
                         <img src="<?php the_sub_field('image'); ?>" alt="" class="img-fluid">
                         <p class="small mt-2"><?php the_sub_field('image_attribution'); ?></p>
                       </div>
-                      <div class="col-lg-6 order-lg-1">
+                      <div class="col-lg-9 order-lg-1">
                         <div class="py-5 px-lg-5">
                           <h4 class="text-primary h3 my-4 mt-lg-0"><?php the_sub_field('header'); ?></h4>
                           <div class="text-muted mb-5">
                             <?php the_sub_field('content'); ?>
                           </div>
                           <?php $section_right_link = get_sub_field('link'); ?>
-                          <a href="<?= $section_right_link['url']; ?>" class="btn btn-outline-primary-light btn-lg mb-4 mb-lg-0">
-                            <?= $section_right_link['title']; ?>
-                          </a>
+                          <?php if($section_right_link): ?>
+                            <a href="<?= $section_right_link['url']; ?>" class="btn btn-outline-primary-light btn-lg mb-4 mb-lg-0">
+                              <?= $section_right_link['title']; ?>
+                            </a>
+                          <?php endif; ?>
                         </div>
                       </div>
                     </div>
--- docroot/content/themes/ddevcom_theme/template-quickstart.php
+++ docroot/content/themes/ddevcom_theme/template-quickstart.php
@@ -11,7 +11,7 @@
     <div class="row">
       <div class="col-lg-6 col-sm-8 col-md-7 mx-auto">
         <div class="py-lg-5">
-          <h1 class="text-primary mt-lg-5">Get Started with DDEV</h1>
+          <h1 class="text-primary mt-lg-5 mb-4">Get Started with DDEV</h1>
           <div class="wysiwyg">
             <?php the_content(); ?>
           </div>
