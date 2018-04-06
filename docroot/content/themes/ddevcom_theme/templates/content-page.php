e4962b4ddf2fce3f7022f3344cac5b170aa68f06
Jed Darrohn <jamesdarrohn@gmail.com>
2018-04-06 08:43:30 -0600

    Clean up basic page

--- docroot/content/themes/ddevcom_theme/templates/content-page.php
+++ docroot/content/themes/ddevcom_theme/templates/content-page.php
@@ -1,2 +1,11 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<div class="container py-5">
+  <div class="row">
+    <div class="col-lg-9">
+      <div class="wysiwyg">
+        <?php the_content(); ?>
+        <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
--- docroot/content/themes/ddevcom_theme/templates/page-header.php
+++ docroot/content/themes/ddevcom_theme/templates/page-header.php
@@ -1,9 +1,9 @@
 <?php use Roots\Sage\Titles; ?>
 <section class="page-header-jumbotron">
   <div class="jumbotron bg-primary-gradient rounded-0 mb-0">
-    <div class="container-fluid">
+    <div class="container">
       <div class="row">
-        <div class="col-lg-9 mx-auto">
+        <div class="col-lg-9">
           <header>
             <h1 class="text-white"><?php the_title(); ?></h1>
             <div class="lead text-white"><?php the_field('page_header_subheader'); ?></div>
