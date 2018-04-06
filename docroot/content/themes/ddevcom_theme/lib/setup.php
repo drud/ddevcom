01894cfd7f259983b18b540d91bbde387bfed2d5
Jed Darrohn <jamesdarrohn@gmail.com>
2018-04-06 08:26:23 -0600

    Added back 4th footer widget area

--- docroot/content/themes/ddevcom_theme/lib/setup.php
+++ docroot/content/themes/ddevcom_theme/lib/setup.php
@@ -91,6 +91,15 @@ function widgets_init() {
   ]);
 
   register_sidebar([
+    'name'          => __('Footer 4', 'sage'),
+    'id'            => 'footer-4',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ]);
+
+  register_sidebar([
     'name'          => __('Front Page Events', 'sage'),
     'id'            => 'front-page-events',
     'before_widget' => '<section class="widget %1$s %2$s">',
