c67c2fc8dc300a9c147b96fc8af6a39ae17c2b79
Jed Darrohn <jed@Jeds-MacBook-Pro.local>
2018-04-26 11:10:34 -0600

    Enqueue Twitter's Javascript

--- docroot/content/themes/ddevcom_theme/lib/extras.php
+++ docroot/content/themes/ddevcom_theme/lib/extras.php
@@ -31,3 +31,7 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+add_action('wp_enqueue_scripts', function(){
+  wp_enqueue_script('twitter-js-widgets','https://platform.twitter.com/widgets.js');
+}, 100);
