c63b63217fcae0d2dc2179bd418376edee133e45
Jed Darrohn <jed@Jeds-MacBook-Pro.local>
2018-05-11 10:39:56 -0600

    Add the featured image or social media image to the post cards throughout website

--- docroot/content/themes/ddevcom_theme/templates/content-card.php
+++ docroot/content/themes/ddevcom_theme/templates/content-card.php
@@ -1,8 +1,17 @@
 <div class="card rounded-0 border-0 bg-white py-5 text-center text-lg-left">
   <article class="persona-article">
+
+    <?php
+      if(get_field('_social_image_url')) {
+        $card_image_url = get_field('_social_image_url');
+      } else {
+        $card_image_url = get_the_post_thumbnail_url();
+      }
+    ?>
+
+    <img class="card-img-top mb-4 rounded-0" src="<?= $card_image_url; ?>" alt="Card image cap">
     <header>
       <div class="card-header border-0 py-0 bg-white">
-
           <h3 class="mb-3">
             <a href="<?php the_permalink(); ?>" class="text-primary-light"><?php the_title(); ?></a>
           </h3>
