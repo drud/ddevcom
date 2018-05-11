734e4d3d98b072be9a05c19d00c635f8b05bd383
Jed Darrohn <jed@Jeds-MacBook-Pro.local>
2018-05-11 09:15:41 -0600

    Add conditional check on whether to display the cookie warning or not

--- docroot/content/themes/ddevcom_theme/templates/alert-cookies.php
+++ docroot/content/themes/ddevcom_theme/templates/alert-cookies.php
@@ -1,12 +1,14 @@
-<div id="cookie-alert" class="alert alert-warning alert-dismissible fade mb-0 py-4 px-lg-4 rounded-0" role="alert">
-    <div class="row">
-        <div class="col-lg-10">
-            <div class="small">
-                <?php the_field('cookie_warning_content', 'option'); ?>
+<?php if(get_field('cookie_display', 'option')): ?>
+    <div id="cookie-alert" class="alert alert-warning alert-dismissible fade mb-0 py-4 px-lg-4 rounded-0" role="alert">
+        <div class="row">
+            <div class="col-lg-10">
+                <div class="small">
+                    <?php the_field('cookie_warning_content', 'option'); ?>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <a href="#" id="accept-cookies" class="btn btn-outline-primary d-block" class="close" data-dismiss="alert" aria-label="Close">Accept Cookies</a>
             </div>
         </div>
-        <div class="col-lg-2">
-            <a href="#" id="accept-cookies" class="btn btn-outline-primary d-block" class="close" data-dismiss="alert" aria-label="Close">Accept Cookies</a>
-        </div>
     </div>
-</div>
+<?php endif; ?>
