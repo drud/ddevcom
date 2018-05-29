fab49bc29df7468ba4da5c9c79ffca1a136c995c
Nathan Smith <webalkymst@gmail.com>
2018-05-29 16:03:41 -0600

    added custom config settings for the content dir (#63)
    * Update Company Name footer
    
    * DDEV local needed updates
    
    * added custom config settings for the content dir

---
+++ docroot/wp-config-drud.php
@@ -0,0 +1,7 @@
+<?php
+
+// full local path of current directory (no trailing slash)
+define('WP_CONTENT_DIR', getcwd().'/content');
+
+// full URI of current directory (no trailing slash)
+define('WP_CONTENT_URL', '/content');
