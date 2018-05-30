297c187e1b4a3d03a26828eafdc83ef24719ba50
alkymst <webalkymst@gmail.com>
2018-05-30 17:31:41 -0600

    add die statement to index.php to force it to die and not move on

--- docroot/index.php
+++ docroot/index.php
@@ -1,4 +1,7 @@
 <?php
+echo "We made it to the index.php!";
+die;
+
 // WordPress view bootstrapper
 define('WP_USE_THEMES', true);
-require(dirname( __FILE__ ) . '/wp/wp-blog-header.php');
+require(dirname(__FILE__) . '/wp/wp-blog-header.php');
