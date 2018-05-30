6c6e67230efc8a22d06f82aa1f8228f1117bcf44
Cameron Eagans <me@cweagans.net>
2018-05-30 11:21:30 -0600

    Build the WP_HOME URL from the request

--- docroot/wp-config-drud.php
+++ docroot/wp-config-drud.php
@@ -1,13 +1,13 @@
 <?php
 
 // site URL
-define('WP_HOME', 'http://drud-com-production.site-prod.ddev-live.drud.io');
+define('WP_HOME', ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']);
 
 // WP URL
-define('WP_SITEURL', 'http://drud-com-production.site-prod.ddev-live.drud.io/wp');
+define('WP_SITEURL', WP_HOME . '/wp');
 
 // full local path of current directory (no trailing slash)
-define('WP_CONTENT_DIR', getcwd().'/content');
+define('WP_CONTENT_DIR', getcwd() . '/content');
 
 // full URI of current directory (no trailing slash)
 define('WP_CONTENT_URL', '/content');
