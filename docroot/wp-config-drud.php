2e6c827679ffa51e7a652a1c535b68acc53ccbf3
Nathan Smith <webalkymst@gmail.com>
2018-05-30 12:25:19 -0600

    updates for local and remote settings (#66)
    * Update Company Name footer
    
    * DDEV local needed updates
    
    * added custom config settings for the content dir
    
    * updates for local and remote settings
    
    * Build the WP_HOME URL from the request
    
    * remove wp-config-local.php

--- docroot/wp-config-drud.php
+++ docroot/wp-config-drud.php
@@ -1,10 +1,13 @@
 <?php
 
-// testing
-define('ABSPATH', dirname(__FILE__) . '/wp/');
+// site URL
+define('WP_HOME', ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']);
+
+// WP URL
+define('WP_SITEURL', WP_HOME . '/wp');
 
 // full local path of current directory (no trailing slash)
-define('WP_CONTENT_DIR', getcwd().'/content');
+define('WP_CONTENT_DIR', getcwd() . '/content');
 
 // full URI of current directory (no trailing slash)
 define('WP_CONTENT_URL', '/content');
