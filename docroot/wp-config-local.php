9796c320e6c5096053bddf08523e5052b0397304
alkymst <webalkymst@gmail.com>
2018-05-30 10:44:02 -0600

    updates for local and remote settings

---
+++ docroot/wp-config-local.php
@@ -0,0 +1,13 @@
+<?php
+
+// site URL
+define('WP_HOME', 'http://ddevwebsite.ddev.local');
+
+// WP URL
+define('WP_SITEURL', 'http://ddevwebsite.ddev.local/wp');
+
+// full local path of current directory (no trailing slash)
+define('WP_CONTENT_DIR', getcwd().'/content');
+
+// full URI of current directory (no trailing slash)
+define('WP_CONTENT_URL', '/content');
