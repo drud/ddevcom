0adbf11288e257a25e5d1763818a062b1d34c1c9
alkymst <webalkymst@gmail.com>
2018-06-06 11:13:54 -0600

    test updating auth keys

--- docroot/wp-config.php
+++ docroot/wp-config.php
@@ -17,10 +17,17 @@ define('DB_CHARSET', 'utf8');
 
 define('DB_COLLATE', '');
 
-define('AUTH_KEY', getenv('WPAUTHKEY'));
-define('SECURE_AUTH_KEY', getenv('WPSECUREAUTHKEY'));
-define('LOGGED_IN_KEY', getenv('WPLOGGEDINKEY'));
-define('NONCE_KEY', getenv('WPNONCEKEY'));
+// define('AUTH_KEY', getenv('WPAUTHKEY'));
+// define('SECURE_AUTH_KEY', getenv('WPSECUREAUTHKEY'));
+// define('LOGGED_IN_KEY', getenv('WPLOGGEDINKEY'));
+// define('NONCE_KEY', getenv('WPNONCEKEY'));
+
+define('AUTH_KEY', 'jE|Jl3pjPG+9~sUy;<n*$#zjKjniWS{PH)L34@@|[EBrt`SSk0GH^M&=?H>ba2qt');
+define('SECURE_AUTH_KEY', 'b;:s]q%hS2sfTxY;yDD^]qH*@x9-<p&,r+!1Jrh[FFXHo,/:!vf8WrRo<zd52Rn3');
+define('LOGGED_IN_KEY', 'I=VWWODrr( D3r/R$Z|2m/shWNd+aEIX{5 -7M<Zy62%CO2JSdT+l-M[DDFdq*}=');
+define('NONCE_KEY', 'A|ex+.V> ]1/z37wj:}$UpZ<+J4~C{svp}xYTZ^VX>GOqgIG8XKRJ>{?h+}DDGVL');
+
+
 define('AUTH_SALT', getenv('WPAUTHSALT'));
 define('SECURE_AUTH_SALT', getenv('WPSECUREAUTHSALT'));
 define('LOGGED_IN_SALT', getenv('WPLOGGEDINSALT'));
