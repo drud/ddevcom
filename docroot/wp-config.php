973ec0bcc7b1189d7c78d76c9794958bc63d2b38
alkymst <webalkymst@gmail.com>
2018-06-06 12:02:08 -0600

    reset config file

--- docroot/wp-config.php
+++ docroot/wp-config.php
@@ -26,11 +26,10 @@ define('SECURE_AUTH_SALT', getenv('WPSECUREAUTHSALT'));
 define('LOGGED_IN_SALT', getenv('WPLOGGEDINSALT'));
 define('NONCE_SALT', getenv('WPNONCESALT'));
 
-
 $table_prefix  = 'wp_';
 
 // site URL
-define('WP_HOME', 'https://www.drud.com');
+define('WP_HOME', ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']);
 
 // WP URL
 define('WP_SITEURL', WP_HOME . '/wp');
@@ -41,6 +40,8 @@ define('WP_CONTENT_DIR', getcwd() . '/content');
 // full URI of current directory (no trailing slash)
 define('WP_CONTENT_URL', WP_HOME . '/content');
 
+// force SSL for admin
+define('FORCE_SSL_ADMIN', true);
 
 define('WP_DEBUG', false);
 define('DISALLOW_FILE_MODS', true);
