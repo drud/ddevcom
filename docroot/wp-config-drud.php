f35beac04e85d22237944c1d0b5fa45d10bbe8e5
alkymst <webalkymst@gmail.com>
2018-06-18 13:51:54 -0600

    some cleanup

--- docroot/wp-config-drud.php
+++ docroot/wp-config-drud.php
@@ -7,22 +7,26 @@ if (getenv('DDEV_ENV_NAME') == 'production') {
     define('WP_HOME', ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']);
 }
 
+/** WP in Sub-directory */
 // WP URL
 define('WP_SITEURL', WP_HOME . '/wp');
+// set WP ABSPATH
+define('ABSPATH', getenv('NGINX_DOCROOT') . '/wp/');
 
+/** Custom wp-content Directory */
 // full local path of current directory (no trailing slash)
 define('WP_CONTENT_DIR', getenv('NGINX_DOCROOT') . '/content');
-
 // full URI of current directory (no trailing slash)
 define('WP_CONTENT_URL', WP_HOME . '/content');
-
-// force SSL for admin
-define('FORCE_SSL_ADMIN', true);
-
-define('WP_DEBUG', false);
-define('DISALLOW_FILE_MODS', true);
 
 // placeholder for WP_ENV - force indexing
 define('WP_ENV', getenv('DDEV_ENV_NAME') ? getenv('DDEV_ENV_NAME') : 'production');
 
-define('ABSPATH', getenv('NGINX_DOCROOT') . '/wp/');
+// force SSL for admin
+define('FORCE_SSL_ADMIN', true);
+
+// turn off debugging
+define('WP_DEBUG', false);
+
+// dis-allow file editing from the admin
+define('DISALLOW_FILE_MODS', true);
