22c5040326fa4291a5087401b879aae46e055726
alkymst <webalkymst@gmail.com>
2018-10-05 10:02:08 -0600

    remove old GDPR notice and setup new one

--- docroot/content/themes/ddevcom_theme/templates/head.php
+++ docroot/content/themes/ddevcom_theme/templates/head.php
@@ -2,23 +2,5 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <?php if(get_field('company_google_analytics_snippet', 'option')): ?>
-    <?php the_field('company_google_analytics_snippet', 'option');  ?>
-  <?php endif; ?>
-
   <?php wp_head(); ?>
-
-  <!-- Hotjar Tracking Code for www.drud.com -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:832603,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-
 </head>
