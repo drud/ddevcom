0d246d02526151fbd2b3113d67cee06460a2c351
alkymst <webalkymst@gmail.com>
2018-10-09 11:38:39 -0600

    move settings files to uploads

--- docroot/wp-config-drud.php
+++ docroot/wp-config-drud.php
@@ -35,7 +35,7 @@ define('DISALLOW_FILE_MODS', true);
 define('WP_CACHE', true); // Added by W3 Total Cache
 define('W3TC_CACHE_DIR', WP_CONTENT_DIR .  '/uploads/cache');
 define('W3TC_CONFIG_DIR', WP_CONTENT_DIR .  '/uploads/w3tc-config');
-// define('W3TC_ADDIN_FILE_ADVANCED_CACHE', '/temp/advanced-cache.php');
-// define('W3TC_ADDIN_FILE_DB', '/temp/db.php');
-// define('W3TC_FILE_DB_CLUSTER_CONFIG', '/temp/db-cluster-config.php');
-// define('W3TC_ADDIN_FILE_OBJECT_CACHE', '/temp/object-cache.php');
+define('W3TC_ADDIN_FILE_ADVANCED_CACHE', WP_CONTENT_DIR .  '/uploads/advanced-cache.php');
+define('W3TC_ADDIN_FILE_DB', WP_CONTENT_DIR .  '/uploads/db.php');
+define('W3TC_FILE_DB_CLUSTER_CONFIG', WP_CONTENT_DIR .  '/uploads/db-cluster-config.php');
+define('W3TC_ADDIN_FILE_OBJECT_CACHE', WP_CONTENT_DIR .  '/uploads/object-cache.php');
