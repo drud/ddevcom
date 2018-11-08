3bc9d8709755fce348c3ea702da979b47b5b857f
alkymst <webalkymst@gmail.com>
2018-11-08 11:26:01 -0700

    disable w3 total cache

--- docroot/wp-config-drud.php
+++ docroot/wp-config-drud.php
@@ -20,7 +20,7 @@ define('WP_CONTENT_DIR', getenv('NGINX_DOCROOT') . '/content');
 define('WP_CONTENT_URL', WP_HOME . '/content');
 
 // placeholder for WP_ENV - force indexing
-define('WP_ENV', getenv('DDEV_ENV_NAME') ? getenv('DDEV_ENV_NAME') : 'production');
+define('WP_ENV', 'production');
 
 // force SSL for admin
 define('FORCE_SSL_ADMIN', false);
@@ -32,6 +32,6 @@ define('WP_DEBUG', false);
 define('DISALLOW_FILE_MODS', true);
 
 /** Enable W3 Total Cache */
-define('WP_CACHE', true); // Added by W3 Total Cache
+define('WP_CACHE', false); // Added by W3 Total Cache
 define('W3TC_CACHE_DIR', WP_CONTENT_DIR .  '/uploads/cache');
 define('W3TC_CONFIG_DIR', WP_CONTENT_DIR .  '/uploads/w3tc-config');
