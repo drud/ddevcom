7ab0776d6d6d2f44c905d11995173df6f4d20a2c
alkymst <webalkymst@gmail.com>
2018-11-07 18:30:48 -0700

    fix video modal bug

--- docroot/content/themes/ddevcom_theme/templates/modal-video.php
+++ docroot/content/themes/ddevcom_theme/templates/modal-video.php
@@ -5,12 +5,12 @@
         <div class="row">
             <div class="col-lg-8 mx-auto">
                 <div class="py-5">
-                    <img src="/content/themes/ddevcom_theme/dist/images/modal-close.svg" alt="" class="close" data-dismiss="modal" width="20">                
+                    <img src="/content/themes/ddevcom_theme/dist/images/modal-close.svg" alt="" class="close" data-dismiss="modal" width="20">
                 </div>
                 <div class="video-wrapper">
-                    <iframe width="560" height="315" src="<?php the_field('front_page_video_embed_url'); ?>?modestbranding=1&showinfo=0&rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="<?php the_field('front_page_video_embed_url', 33); ?>?modestbranding=1&showinfo=0&rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div>
-                <button type="button" class="btn btn-outline-primary d-block mt-3 mx-auto" data-dismiss="modal">Close</button>    
+                <button type="button" class="btn btn-outline-primary d-block mt-3 mx-auto" data-dismiss="modal">Close</button>
             </div>
         </div>
       </div>
