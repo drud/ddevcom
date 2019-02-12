2151f8bd75cfc7ddd33a61951916eeeb41b2bc8e
alkymst <webalkymst@gmail.com>
2019-02-12 12:50:59 -0700

    add field to the banner to amke manageable

--- docroot/content/themes/ddevcom_theme/templates/landing-jumbotron.php
+++ docroot/content/themes/ddevcom_theme/templates/landing-jumbotron.php
@@ -8,16 +8,26 @@
               <?php the_field('product_jumbotron_header'); ?>
             </h1>
           </header>
+          <?php
+          if (get_field('product_jumbotron_content')) {
+              ?>
+          <div class="text-white">
+            <?php the_field('product_jumbotron_content'); ?>
+          </div>
+          <?php
+          } ?>
           <?php $button_1_link = get_field('product_jumbotron_button_1_link'); ?>
           <?php if ($button_1_link) : ?>
-          <a href="<?= $button_1_link['url']; ?>" class="btn btn-outline-secondary btn-lg my-1 px-5">
+          <a href="<?= $button_1_link['url']; ?>"
+            class="btn btn-outline-secondary btn-lg my-1 px-5">
             <?= $button_1_link['title']; ?>
           </a>
           <?php endif; ?>
 
           <?php $button_2_link = get_field('product_jumbotron_button_2_link'); ?>
           <?php if ($button_2_link) : ?>
-          <a href="<?= $button_2_link['url']; ?>" class="btn btn-outline-light btn-lg">
+          <a href="<?= $button_2_link['url']; ?>"
+            class="btn btn-outline-light btn-lg">
             <?= $button_2_link['title']; ?>
           </a>
           <?php endif; ?>
