42bf3289fcdd5e8cf158ec68b7d75b5509a605bb
Nathan Smith <webalkymst@gmail.com>
2019-09-30 12:22:55 -0600

    remove w3 total cache

---
+++ docroot/content/wp-rocket-config/drud-com.ddev.site.php
@@ -0,0 +1,18 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+$rocket_cookie_hash = 'cc4e45e49018f4115e602d88348140a1';
+$rocket_cache_mobile_files_tablet = 'desktop';
+$rocket_cache_mobile = '1';
+$rocket_cache_ssl = '1';
+$rocket_cache_reject_uri = '/(.+/)?feed/?|/(index\.php/)?wp\-json(/.*|$)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array (
+  0 => 'q',
+);
+$rocket_secret_cache_key = '5be5f4e1aea0f656449656';
+$rocket_do_caching_mobile_files = '0';
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array (
+);
