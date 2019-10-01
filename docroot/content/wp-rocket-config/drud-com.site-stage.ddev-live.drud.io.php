05260c6941e177b58100f2b67df4352a751e7dec
Nathan Smith <webalkymst@gmail.com>
2019-10-01 12:37:07 -0600

    update staging wprocket config

--- docroot/content/wp-rocket-config/drud-com.site-stage.ddev-live.drud.io.php
+++ docroot/content/wp-rocket-config/drud-com.site-stage.ddev-live.drud.io.php
@@ -1,18 +1,18 @@
 <?php
-defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+defined('ABSPATH') || die('Cheatin&#8217; uh?');
 
-$rocket_cookie_hash = '76fecd6143e20653ad9eff111d410373';
+$rocket_cookie_hash = 'cc4e45e49018f4115e602d88348140a1';
+$rocket_logged_in_cookie = 'wordpress_logged_in_cc4e45e49018f4115e602d88348140a1';
 $rocket_cache_mobile_files_tablet = 'desktop';
-$rocket_cache_mobile = '1';
-$rocket_cache_ssl = '1';
-$rocket_cache_reject_uri = '/(.+/)?feed/?|/(index\.php/)?wp\-json(/.*|$)';
-$rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_mobile = 1;
+$rocket_cache_ssl = 1;
+$rocket_cache_reject_uri = '/(.+/)?feed/?|/(?:.+/)?embed/|/(index\.php/)?wp\-json(/.*|$)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_.+|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
 $rocket_cache_reject_ua = 'facebookexternalhit';
-$rocket_cache_query_strings = array (
+$rocket_cache_query_strings = array(
   0 => 'q',
 );
-$rocket_secret_cache_key = '5be4952a42a36988324420';
-$rocket_do_caching_mobile_files = '0';
+$rocket_secret_cache_key = '5be5f4e1aea0f656449656';
+$rocket_do_caching_mobile_files = 0;
 $rocket_cache_mandatory_cookies = '';
-$rocket_cache_dynamic_cookies = array (
-);
+$rocket_cache_dynamic_cookies = array();
