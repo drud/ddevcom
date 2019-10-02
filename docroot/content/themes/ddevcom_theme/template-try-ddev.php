f083eb2b6fdc638bd111f1ffddf660d25e048534
Nathan Smith <webalkymst@gmail.com>
2019-10-01 23:34:26 -0600

    try ddev template updates

--- docroot/content/themes/ddevcom_theme/template-try-ddev.php
+++ docroot/content/themes/ddevcom_theme/template-try-ddev.php
@@ -3,26 +3,25 @@
  * Template Name: Try DDEV
  */
 
-while (have_posts()) : the_post();
-  // jumbotron
-  get_template_part('templates/landing', 'jumbotron'); ?>
+while (have_posts()) : the_post(); ?>
 
-<section class="landing-signup bg-light">
-  <div class="container-fluid py-1 bg-secondary"></div>
-  <div class="container py-5">
-    <div class="row py-5">
-      <div class="col-md-12">
-        <?php the_content(); ?>
-      </div>
-      <div class="col-md-12 offset-md-1">
-        <div class="p-5">
-          <h2 class="my-5">Sign Up Now</h2>
-          <?php gravity_form(8, false, false, false, null, true);?>
+<?php get_template_part('templates/page', 'header'); ?>
+
+<section>
+  <div class="container-fluid py-5">
+    <div class="row">
+      <div class="col-lg-6 col-sm-8 col-md-7 mx-auto">
+        <div class="py-lg-5">
+          <header>
+            <h1 class="text-primary mt-lg-5 my-4">Get Started with DDEV</h1>
+          </header>
+          <div class="wysiwyg">
+            <?php the_content(); ?>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </section>
 
-<?php
-endwhile;
+<?php endwhile;
