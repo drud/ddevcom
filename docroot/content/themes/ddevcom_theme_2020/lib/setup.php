43048cdbcc20399307ebe26454521d017b97dc98
Jed Darrohn <jamesdarrohn@gmail.com>
2020-05-21 13:29:52 -0600

    Building out the product template

--- docroot/content/themes/ddevcom_theme_2020/lib/setup.php
+++ docroot/content/themes/ddevcom_theme_2020/lib/setup.php
@@ -37,6 +37,7 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
   // http://codex.wordpress.org/Function_Reference/add_image_size
   add_theme_support('post-thumbnails');
+  add_image_size('post-card-header', 600, 400, true);
 
   // Enable post formats
   // http://codex.wordpress.org/Post_Formats
