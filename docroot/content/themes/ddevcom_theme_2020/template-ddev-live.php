57cd0d61b9a639397a42cd47d3632889c35cd1c6
James Darrohn <jamesdarrohn@gmail.com>
2020-05-26 22:06:45 -0600

    Wrapping up most of product pages. Small items remaining for morning (#137)

--- docroot/content/themes/ddevcom_theme_2020/template-ddev-live.php
+++ docroot/content/themes/ddevcom_theme_2020/template-ddev-live.php
@@ -39,7 +39,7 @@
             Host Drupal, WordPress and TYPO3 sites on scalable, flexible infrastructure that just works. DDEV-Live reduces the complexity of hosting modern websites so you can focus on delivering great projects using your favorite workflows.
           </p>
           <div class="mb-5">
-            <a href="http://dash.ddev.com/account/create" class="btn btn-success btn-lg mr-2">Start a Free Trial</a>
+            <a href="https://ddev-live-qa.firebaseapp.com/account/create" class="btn btn-success btn-lg mr-2">Start a Free Trial</a>
             <a href="https://docs.ddev.com" class="btn btn-outline-success btn-lg">Documentation</a>
           </div>
         </div>
@@ -172,7 +172,7 @@
             </div>
             <div class="py-5 mb-lg-5 bg-primary-dark">
                 <div class="col text-center">
-                  <a href="#" class="btn btn-success btn-lg mr-2">Start a Free Trial</a>
+                  <a href="https://ddev-live-qa.firebaseapp.com/account/create" class="btn btn-success btn-lg mr-2">Start a Free Trial</a>
                   <a href="#" class="btn btn-outline-light btn-lg">Request Demo</a>
                 </div>
               </div>
@@ -249,7 +249,7 @@
               </div>
               <div class="row mt-5">
                 <div class="col text-lg-center">
-                  <a href="#" class="btn btn-lg btn-success mr-2">
+                  <a href="https://ddev-live-qa.firebaseapp.com/account/create" class="btn btn-lg btn-success mr-2">
                     Start a Free Trial
                   </a>
                   <a href="#" class="btn btn-lg btn-outline-success">
