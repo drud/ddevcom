332cb8f7e5683e467f66760afcfc2be70aef84ae
James Darrohn <jamesdarrohn@gmail.com>
2020-05-29 14:31:03 -0600

    Cleaned up the author archive (#162)

--- docroot/content/themes/ddevcom_theme_2020/author.php
+++ docroot/content/themes/ddevcom_theme_2020/author.php
@@ -1,37 +1,36 @@
 <section class="category-posts">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-9 mx-auto">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <div class="pt-lg-5 mt-lg-5 mb-lg-5">
-            <?php
-              $current_author = get_query_var('author_name') ? get_user_by('slug', get_query_var('author_name')) : get_userdata(get_query_var('author'));
-            ?>
-              <div class="d-block mt-lg-5 mb-4">
-                <img class="rounded-circle" src="<?= get_avatar_url($current_author->data->id); ?>" alt="<?= $current_author->data->display_name; ?>">
-                <h1 class="text-primary d-inline"><?= $current_author->data->display_name; ?></h1>
-              </div>
-              <p class="text-muted"><?= get_the_author_description($current_author->data->id); ?></p>
-            </div>
+  <div class="container py-5">
+
+
+      <?php
+        $current_author = get_query_var('author_name') ? get_user_by('slug', get_query_var('author_name')) : get_userdata(get_query_var('author'));
+      ?>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="mb-4">
+            <img class="rounded-circle" src="<?php echo get_avatar_url($current_author->data->ID); ?>" alt="<?= get_the_author_meta('display_name'); ?>">
+            <h1 class="text-primary d-inline"><?= $current_author->data->display_name; ?></h1>
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <h3 class="text-primary-light mb-4">Posts by <?= $current_author->data->display_name; ?>:</h3>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-              <!-- post -->
-              <?php get_template_part('templates/content'); ?>
-            <?php endwhile; ?>
-            <div class="py-5">
-              <?php the_posts_pagination(); ?>
-            </div>
-          <?php else: ?>
-            <!-- no posts found -->
-          <?php endif; ?>
-          </div>
+        <div class="col-lg-8">
+            <p class="text-muted"><?= get_the_author_meta('description', $current_author->data->ID); ?></p>
         </div>
       </div>
-    </div>
+
+      <?php if (have_posts()) : ?>
+        <h3 class="text-dark mb-4">Posts by <?= $current_author->data->display_name; ?>:</h3>
+        <div class="row">
+          <?php while (have_posts()) : the_post(); ?>
+            <div class="col-lg-4 d-flex">
+                <?php get_template_part('templates/content', 'card'); ?>
+            </div>
+          <?php endwhile; ?>
+        </div>
+        <div class="mb-5">
+          <?php the_posts_pagination(); ?>
+        </div>
+      <?php endif; ?>
   </div>
 </section>
+
