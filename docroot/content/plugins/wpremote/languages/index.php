471127f0bc77760adfefd97c35d21cd6199514e1
Jed Darrohn <jamesdarrohn@gmail.com>
2021-01-06 10:47:31 -0700

    Removing the WP Remote plugin

--- docroot/content/plugins/wpremote/languages/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Do not put custom translations here. They will be deleted when you update the WP Remote plugin!
- *
- * Keep custom translations for WP Remote Plugin For WordPress in '/wp-content/languages/wpremote/'
- */
