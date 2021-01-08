e69ce878e1e6c3d3cfe01c3691e23a4372b32a85
Jed Darrohn <jamesdarrohn@gmail.com>
2021-01-08 12:25:19 -0700

    Adding search

--- docroot/content/themes/ddevcom_theme_2020/home.php
+++ docroot/content/themes/ddevcom_theme_2020/home.php
@@ -8,6 +8,21 @@
         <div class="row">
           <div class="col-lg-8">
             <div class="row">
+              <div class="col">
+                <form action="/" class="mb-4">
+                  <div class="input-group input-group-lg mb-2">
+                    <span class="input-group-addon" id="sizing-addon1">
+                      <i class="fa fa-search"></i>
+                    </span>
+                    <input type="text" class="form-control" name="s"/>
+                    <span class="input-group-btn">
+                      <button class="btn btn-secondary" type="button">Search</button>
+                    </span>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="row">
               <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <!-- post -->
                 <div class="col-lg-6 d-flex">
