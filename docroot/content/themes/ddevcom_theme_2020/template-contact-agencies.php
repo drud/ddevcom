6fe91a21373529d3ed7b2c86b1af78be30251bfa
James Darrohn <jdarrohn@ddev.com>
2021-02-22 12:42:25 -0700

    Revert "Revert "Qa""

---
+++ docroot/content/themes/ddevcom_theme_2020/template-contact-agencies.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name: Agencies Contact Template
+ */
+?>
+
+<section class="contact-agencies py-5">
+    <h1>Save time and money while focusing on the core aspects of your business.</h1>
+</section>
