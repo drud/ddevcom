714442f96e6cabd1828f3ae759e6f139dae7d528
Jed Darrohn <jamesdarrohn@gmail.com>
2021-02-25 10:10:42 -0700

    Directory for WordFence logs

---
+++ docroot/content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614876665;s:7:"authKey";s:64:"{q2Nq_9$,!L;0>l9py#k|EC[w$[3iDZt6b8b0C|BTJ8[;bq<U^upS*GZ^,vyI)cc";s:7:"version";s:5:"1.0.4";}
