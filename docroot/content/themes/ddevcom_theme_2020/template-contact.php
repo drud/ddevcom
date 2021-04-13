bd650bf8fe04dd37160b76397fd872a7e11bd03d
Elli Ludwigson <eludwigson@ddev.com>
2021-04-13 18:59:18 -0400

    Update template-contact.php
    Realigning layout

--- docroot/content/themes/ddevcom_theme_2020/template-contact.php
+++ docroot/content/themes/ddevcom_theme_2020/template-contact.php
@@ -5,15 +5,15 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+
+<?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages([
     'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'),
     'after' => '</p></nav>'
   ]); ?>
 <?php endwhile; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
 
   <div class="container py-5">
     <div class="row">
